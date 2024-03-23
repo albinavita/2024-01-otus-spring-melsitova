44e2edc986124022af5cb932a5345927a115eb78
Albina <albina.melsitova@mail.ru>
2024-03-24 02:47:31 +0300

    Чтение и вывод вопросов в консоль.

---
+++ hw01-xml-config/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTect.java
@@ -0,0 +1,19 @@
+package ru.otus.hw.dao;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.otus.hw.config.AppProperties;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CsvQuestionDaoTect {
+
+    @DisplayName("Ошибка файл не найден")
+    @Test
+    void checkNotFoundFileTest() {
+        QuestionDao dao = new CsvQuestionDao(
+                new AppProperties("file_name.csv"));
+        assertThrows(QuestionReadException.class, dao::findAll);
+    }
+}
