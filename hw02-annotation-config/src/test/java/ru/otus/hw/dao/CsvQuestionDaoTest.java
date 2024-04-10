bab4d8ffa4b032e6d0095f9f5a0573414d608687
Albina <albina.melsitova@mail.ru>
2024-04-11 01:20:33 +0300

    Добавила аннотацию и дописала тесты

--- hw02-annotation-config/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw02-annotation-config/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -9,9 +9,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.exceptions.QuestionReadException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
+
 
 @ExtendWith(MockitoExtension.class)
 class CsvQuestionDaoTest {
@@ -39,6 +41,10 @@ class CsvQuestionDaoTest {
         given(appProperties.getTestFileName()).willReturn("question_test.csv");
 
         assertDoesNotThrow(() -> dao.findAll());
+        assertThat(dao.findAll().size()).isEqualTo(2);
+        assertThat(dao.findAll().get(0)).isNotNull();
+        assertThat(dao.findAll().get(0).answers().size()).isEqualTo(2);
+
     }
 
 }
